42352609aa1f57cdb39932c97cf4bc60f83b7a3b
Дмитрий Чехонадских <loseex@vk.com>
2024-07-17 23:27:47 +0500

    Написал общую модель для объявлений и черновиков. Так же добавил репозиторий для взаимодействия с бд.

---
+++ src/main/kotlin/ru/itcollege/feedservice/core/domain/models/entities/General.kt
@@ -0,0 +1,34 @@
+package ru.itcollege.feedservice.core.domain.models.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import org.hibernate.annotations.UuidGenerator
+import ru.itcollege.feedservice.core.domain.models.enums.GStatus
+import java.time.ZonedDateTime
+import java.util.UUID
+
+@Entity(name = "general")
+class General {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @UuidGenerator
+  @Column(unique = true, nullable = false, updatable = false)
+  lateinit var uuid: UUID
+
+  @Column(unique = false, nullable = false, updatable = true)
+  lateinit var title: String
+
+  @Column(unique = false, nullable = false, updatable = true, columnDefinition = "TEXT")
+  lateinit var content: String
+
+  @Column(unique = false, nullable = false, updatable = false)
+  lateinit var authorId: String
+
+  @Column(unique = false, nullable = false, updatable = true)
+  lateinit var date: ZonedDateTime
+
+  @Column(unique = false, nullable = false, updatable = true)
+  lateinit var status: GStatus
+}
