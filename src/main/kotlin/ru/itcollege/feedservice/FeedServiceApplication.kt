2cdb7b86e68a52511b6d5746f7a3add52fe238b0
Дмитрий Чехонадских <loseex@vk.com>
2024-07-28 14:33:03 +0500

    hot fix. Добавил аннотацию EnableAsync для сервера.

--- src/main/kotlin/ru/itcollege/feedservice/FeedServiceApplication.kt
+++ src/main/kotlin/ru/itcollege/feedservice/FeedServiceApplication.kt
@@ -2,8 +2,10 @@ package ru.itcollege.feedservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
 class FeedServiceApplication
 
 fun main(args: Array<String>) {
