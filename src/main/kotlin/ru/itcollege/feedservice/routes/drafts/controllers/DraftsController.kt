630c53f2cc5d58bf3478ded1e27e0c6a12ce3c38
Дмитрий Чехонадских <loseex@vk.com>
2024-08-08 18:19:59 +0500

    Начал работать над логикой в черновиках.
    - Написал метод для получения черновиков определенного пользователя по токену авторизации.
    - Написал метод для получения определенного черновика по uuid.
    - Написал метод для создания черновика (private).
    - Написал метод для обновления черновика (private).
    - Написал метод для сохранения черновика.
    - Написал метод для архивации черновика.
    - Подключил методы в контроллере DraftsController.
    - Написал dto DraftPayload.

--- src/main/kotlin/ru/itcollege/feedservice/routes/drafts/controllers/DraftsController.kt
+++ src/main/kotlin/ru/itcollege/feedservice/routes/drafts/controllers/DraftsController.kt
@@ -1,13 +1,32 @@
 package ru.itcollege.feedservice.routes.drafts.controllers
 
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import ru.itcollege.feedservice.core.domain.models.entities.General
+import ru.itcollege.feedservice.routes.drafts.models.dto.DraftPayload
 import ru.itcollege.feedservice.routes.drafts.services.DraftsService
 
 @Tag(name = "Черновики")
 @RestController
 @RequestMapping("/drafts")
 class DraftsController(private var draftsService: DraftsService) {
-  // todo
+  @GetMapping
+  fun findAllByAuthorId(): MutableList<General> {
+    return this.draftsService.findAllByAuthorId()
+  }
+
+  @GetMapping("{uuid}")
+  fun findOneByUUID(@PathVariable uuid: String): General {
+    return this.draftsService.findOneByUUID(uuid)
+  }
+
+  @PostMapping("/save")
+  fun saveDraft(@RequestParam(required = false) uuid: String?, @RequestBody body: DraftPayload): General? {
+    return this.draftsService.save(uuid, body)
+  }
+
+  @DeleteMapping("/delete/{uuid}")
+  fun archiveDraft(@PathVariable uuid: String): General? {
+    return this.draftsService.archive(uuid)
+  }
 }
